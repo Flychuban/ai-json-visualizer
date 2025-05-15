d518208556edd14f2b5a1d005bb17ef91dfafa95
Flychuban <kaloanas07@gmail.com>
2025-05-15 14:20:55 +0300

    feat: add PostHog integration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   // Add any other configuration options here
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://eu.i.posthog.com/decide',
+      },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 }
 
 module.exports = nextConfig
